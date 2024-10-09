02d6379764aa3e3cfe25a0492cf80280dd02b193
Aditya Kalambe <adityakalambe20@gmail.com>
2024-10-10 00:45:07 +0530

    Final Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,ReactiveFormsModule, CommonModule, JsonPipe],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ReactiveForm';
+
+  cities:string[] = ["Pune", "Amravati", "Mumbai", "Kolkata"];
+  zips:number[] = [411045, 444606, 444555, 444555, 444555, 444555, 444555];
+
+  studentForm: FormGroup  = new FormGroup({
+    firstName: new FormControl('',[Validators.required]),
+    lastName: new FormControl('',[Validators.required]),
+    age: new FormControl('',[Validators.required]),
+    grade: new FormControl('',[Validators.required]),
+    city: new FormControl('',[Validators.required]),
+    zip: new FormControl('',[Validators.required])
+  });
+
+  studentFormValueFromFrom: any;
+  onSubmit():void{
+      this.studentFormValueFromFrom = this.studentForm.value;
+  }
+  onReset():void{
+    this.studentFormValueFromFrom = undefined;
+  }
+}
